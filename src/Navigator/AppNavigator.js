62b0dead91f7d959f2535dda4a93000289931f13
fadi quader <fadi.quader@gmail.com>
2018-11-15 08:59:54 +0200

    ad react native icons

--- src/Navigator/AppNavigator.js
+++ src/Navigator/AppNavigator.js
@@ -1,4 +1,7 @@
+import * as React from 'react';
+import { TouchableOpacity } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 //
 import { HomeScreen } from '../navigation-example/HomeScreen';
 import { DetailsScreen } from '../navigation-example/DetailsScreen';
@@ -21,5 +24,31 @@ export default createBottomTabNavigator(
   {
     Home: HomeStack,
     Settings: SettingsStack,
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-home`;
+        }
+        else if (routeName === 'Settings') {
+          iconName = `ios-options`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Icon name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+      },
+      tabBarLabel: navigation.state.routeName === 'Home' ? 'Main' : 'Set'
+    }),
+    // tabBarComponent: TouchableOpacity,
+    tabBarOptions: {
+      activeTintColor: 'blue',
+      inactiveTintColor: 'red',
+      // showLabel: false
+      // showIcon: false
+    },
   }
 );
